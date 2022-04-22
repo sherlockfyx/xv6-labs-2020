1c489f711dd1fcf2d1e0a0a548599f6746106bca
fangyx <1057198309@qq.com>
2022-04-22 17:19:42 +0800

    finish thread

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,27 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread++;
+  // while(bstate.nthread < nthread) {
+  //   pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  // }
+  // bstate.round++;
+  // bstate.nthread = 0;
+  // pthread_mutex_unlock(&bstate.barrier_mutex);
+  // pthread_cond_broadcast(&bstate.barrier_cond);
+  if (bstate.nthread < nthread)
+  {
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+  }
+  else
+  {
+    bstate.round++;
+    bstate.nthread = 0;
+    pthread_mutex_unlock(&bstate.barrier_mutex);
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
 }
 
 static void *
